27fa976288903fc72ba33bfbee1c92f41f510c60
Ved Prakash Pandey <75631832+vppandey56@users.noreply.github.com>
2022-11-04 17:10:30 +0530

    Update and rename Leaders in an array to 03_Leaders in an array.cpp

--- 03_Leaders in an array.cpp
+++ 03_Leaders in an array.cpp
@@ -1,4 +1,4 @@
-Given an array A of positive integers. Your task is to find the leaders in the array. An element of array is leader if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader. 
+/*Given an array A of positive integers. Your task is to find the leaders in the array. An element of array is leader if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader. 
 
  
 
@@ -22,7 +22,7 @@ Input:
 n = 5
 A[] = {1,2,3,4,0}
 Output: 4 0
-
+*/
 
 
 class Solution{
